42d7be3e324457464afac7edf00861dc389fb6f2
Aryamitra Chaudhuri <caryamitra@gmail.com>
2022-11-29 01:28:58 +0530

    basic backend setup

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
+
+app.get("/", (req,res)=> {
+    res.send("hello!")
+})
+
+const PORT = 5000;
+const DB_URI = "mongodb+srv://Aryamitra:arya123@dev-inside-db.1b4g9yd.mongodb.net/?retryWrites=true&w=majority"
+
+
+mongoose.connect(DB_URI, { useNewUrlParser: true, useUnifiedTopology: true}).then(()=>{
+    console.log('Database connected!');
+})
+
+app.listen(PORT, ()=> {
+    console.log(`Server running at port ${PORT}!`);
+})
