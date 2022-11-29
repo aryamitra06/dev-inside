d99503a11724fac6744ce3360bacfd3a88188055
Aryamitra Chaudhuri <caryamitra@gmail.com>
2022-11-29 21:57:10 +0530

    jwt added

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -1,31 +1,34 @@
 import User from "../model/User.js";
 import gravatar from "gravatar";
 import bcrypt from 'bcryptjs';
+import jwt from "jsonwebtoken";
 
 export const registerUser = async (req, res) => {
     try {
 
         const { name, email, password } = req.body;
-        
+
         // Get users gravatar
         const avatar = gravatar.url(email, { s: '200', r: 'pg', d: 'mm' });
-        
+
         // See if user exists
         const existingUser = await User.findOne({ email });
-        if(existingUser){
-            return res.status(400).json({ "msg": "User already exists"});
+        if (existingUser) {
+            return res.status(400).json({ "msg": "User already exists" });
         }
 
         // Encrypt password
         const salt = await bcrypt.genSalt(10);
         const encryptedPassword = await bcrypt.hash(password, salt);
 
-        // Return jsonwebtoken
 
         //saving user
-        const user = await new User({name: name, email: email, avatar: avatar, password: encryptedPassword});
+        const user = await new User({ name: name, email: email, avatar: avatar, password: encryptedPassword });
         user.save();
-        return res.status(200).json({ "msg": "Success" });
+
+        // Return jsonwebtoken
+        const token = jwt.sign({"user" : { "email": user.email, "id": user._id }}, "meawmeaw", { expiresIn: "12h" });
+        return res.status(200).json({ "msg": "Success", "token": token });
     } catch (error) {
         return res.status(204).json({ "msg": "Error" });
     }
