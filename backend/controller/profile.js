c3a74123b3c2a72c2372cb11be4637f9dc1644c7
Aryamitra Chaudhuri <caryamitra@gmail.com>
2022-11-30 19:47:48 +0530

    profile addition and edition made

--- backend/controller/profile.js
+++ backend/controller/profile.js
@@ -1,4 +1,5 @@
 import Profile from "../model/Profile.js";
+
 export const getMyProfile = async (req, res) => {
     try {
         const profile = await Profile.findOne({ user: req.user.id }).populate('user', ['name', 'avatar']);
@@ -8,5 +9,47 @@ export const getMyProfile = async (req, res) => {
         return res.json(profile);
     } catch (error) {
         res.status(500).json({ msg: "Server Error" });
+    }
+}
+
+export const createUpdateProfile = async (req, res) => {
+    const { company, website, location, bio, status, githubusername, skills, youtube, facebook, twitter, instagram, linkedin } = req.body;
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if (company) profileFields.company = company;
+    if (website) profileFields.website = website;
+    if (location) profileFields.location = location;
+    if (bio) profileFields.bio = bio;
+    if (status) profileFields.status = status;
+    if (githubusername) profileFields.githubusername = githubusername;
+    if (skills) {
+        profileFields.skills = skills.split(',').map(skill => skill.trim());
+    }
+
+    // Social objects
+    profileFields.social = {};
+    if (youtube) profileFields.social.youtube = youtube;
+    if (facebook) profileFields.social.facebook = facebook;
+    if (twitter) profileFields.social.twitter = twitter;
+    if (instagram) profileFields.social.instagram = instagram;
+    if (linkedin) profileFields.social.linkedin = linkedin;
+
+    try {
+        // If the profile is available only that case user can update otherwise make new one
+        if (Profile.findOne({ user: req.user.id })) {
+            await Profile.findOneAndUpdate(
+                { user: req.user.id },
+                { $set: profileFields },
+                { new: true })
+            return res.json(profileFields);
+        }
+        else {
+            const profile = new Profile(profileFields);
+            await profile.save();
+            return res.json(profileFields);
+        }
+
+    } catch (error) {
+        res.status(404).json({ msg: "Server Error" });
     }
 }
--- backend/service/profile.js
+++ backend/service/profile.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { auth } from "../middleware/auth.js";
-import { getMyProfile } from "../controller/profile.js";
+import { getMyProfile, createUpdateProfile } from "../controller/profile.js";
 const router = express.Router();
 
 /*
@@ -10,5 +10,13 @@ const router = express.Router();
 @desc: Getting profile details
 */
 router.get("/me", auth, getMyProfile);
+
+/*
+@route: /createupdateprofile
+@method: POST
+@access: Private
+@desc: Creating new profile
+*/
+router.post("/createupdateprofile", auth, createUpdateProfile);
 
 export default router;
