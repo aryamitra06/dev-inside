7319ed75f9db07781732ccba774b5b1cd18e8eee
Aryamitra Chaudhuri <caryamitra@gmail.com>
2022-12-02 15:24:31 +0530

    get and delete post api created

--- backend/controller/post.js
+++ backend/controller/post.js
@@ -2,10 +2,10 @@ import Post from "../model/Post.js";
 import Profile from "../model/Profile.js";
 import User from "../model/User.js";
 
-export const addNewPost = async(req,res) => {
+export const addNewPost = async (req, res) => {
     try {
         const user = await User.findById(req.user.id).select('-password');
-    
+
         const newPost = new Post({
             user: req.user.id,
             text: req.body.text,
@@ -15,8 +15,43 @@ export const addNewPost = async(req,res) => {
 
         const post = await newPost.save();
         res.json(post);
-        
+
     } catch (error) {
-        res.status(500).json({msg: "Server error"});
+        res.status(500).json({ msg: "Server error" });
+    }
+}
+
+export const getPosts = async (req, res) => {
+    try {
+        const posts = await Post.find().sort({ date: -1 });
+        res.json(posts);
+    } catch (error) {
+        res.status(500).json({ msg: "Server error" });
+    }
+}
+
+export const getPostById = async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (!post) return res.status(404).json({ msg: "No post found" });
+        return res.json(post);
+    } catch (error) {
+        if (error.kind === "ObjectId") return res.status(404).json({ msg: "No post found" });
+        return res.status(500).json({ msg: "Server error" });
+    }
+}
+
+export const deletePost = async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        if (post.user.toString() !== req.user.id) {
+            return res.status(401).json({ msg: "User not authorized" });
+        }
+        await post.remove();
+        return res.json({ msg: "Post removed" });
+    } catch (error) {
+        if (error.kind === "ObjectId") return res.status(404).json({ msg: "No post found" });
+        return res.status(500).json({ msg: "Server error" });
     }
 }
--- backend/service/post.js
+++ backend/service/post.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addNewPost } from "../controller/post.js";
+import { addNewPost, getPosts, getPostById, deletePost } from "../controller/post.js";
 import { auth } from "../middleware/auth.js";
 
 const router = express.Router();
@@ -11,5 +11,29 @@ const router = express.Router();
 @desc: Creating new post
 */
 router.post("/newpost", auth, addNewPost);
+
+/*
+@route: /posts
+@method: GET
+@access: Private
+@desc: Getting posts
+*/
+router.get("/posts", auth, getPosts);
+
+/*
+@route: /post/:id
+@method: GET
+@access: Private
+@desc: Getting post by id
+*/
+router.get("/post/:id", auth, getPostById);
+
+/*
+@route: /post/delete/:id
+@method: DELETE
+@access: Private
+@desc: Deleting post by id
+*/
+router.delete("/post/delete/:id", auth, deletePost);
 
 export default router;
