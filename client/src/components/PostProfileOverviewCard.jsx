4bf7ef33051804545708012c2354e7a70f1324f2
Aryamitra Chaudhuri <caryamitra@gmail.com>
2022-12-15 11:57:31 +0530

    bugs fixed

--- client/src/components/PostProfileOverviewCard.jsx
+++ client/src/components/PostProfileOverviewCard.jsx
@@ -1,30 +1,53 @@
-import { Avatar, Box, Button, Card, CardBody, Center, Tag, Text } from '@chakra-ui/react'
-import React, { Fragment } from 'react'
-
-export default function PostProfileOverviewCard({data}) {
-  const {response, loading, error} = data;
-  console.log(response);
+import { Avatar, Box, Button, Card, CardBody, Center, Tag, Text, Alert, AlertIcon, Skeleton, Stack } from '@chakra-ui/react'
+import React, { Fragment, memo } from 'react'
+import { Link } from "react-router-dom";
+import moment from 'moment';
+const PostProfileOverviewCard = ({ data }) => {
+  const { response, loading, error } = data;
   return (
     <Fragment>
-      <Card variant={"outline"} borderRadius={"xl"} overflow={"hidden"} mb={5} boxShadow={"lg"}>
-        <Box bgGradient='linear(to-l, #5433FF, #20BDFF)' height={"13px"}/>
-        <Box display={"flex"} alignItems={"center"} flexDirection={"column"} justifyContent={"center"} mt={6}>
-          <Avatar />
-          <Text mt={1} fontSize={"xl"} fontWeight={"bold"}>{response?.user?.name}</Text>
-        </Box>
-        <Center>
-        </Center>
-        <CardBody>
-          <Button colorScheme={"blue"} w={"100%"} mb={3}>View Profile</Button>
-          <Text color={"gray.400"} fontSize={"sm"}>John Doe and Jane Doe are multiple-use placeholder names that are used when the true name of a person is unknown or is being intentionally concealed. In the context of law enforcement in the United States, such names are often used to refer to a corpse whose identity is unknown or unconfirmed.</Text>
-          <Tag mt={3} mb={2}>Location</Tag>
-          <Text color={"gray.400"} fontSize={"sm"}>USA</Text>
-          <Tag mt={3} mb={2}>Work</Tag>
-          <Text color={"gray.400"} fontSize={"sm"}>SDE at Amazon</Text>
-          <Tag mt={3} mb={2}>Joined</Tag>
-          <Text color={"gray.400"} fontSize={"sm"}>16 Oct 2019</Text>
-        </CardBody>
-      </Card>
+      {
+        loading ? (
+          <Stack>
+            <Skeleton height={"200px"}/>
+            <Skeleton height={"100px"}/>
+            <Skeleton height={"70px"}/>
+          </Stack>
+        ) : (
+          <>
+            {
+              error ? (
+                <Alert status='error'>
+                  <AlertIcon />
+                  {error?.msg}
+                </Alert>
+              ) : (
+                <Card variant={"outline"} borderRadius={"xl"} overflow={"hidden"} mb={5} boxShadow={"lg"}>
+                  <Box bgGradient='linear(to-l, #5433FF, #20BDFF)' height={"13px"} />
+                  <Box display={"flex"} alignItems={"center"} flexDirection={"column"} justifyContent={"center"} mt={6}>
+                    <Avatar src={response?.user?.avatar}/>
+                    <Text mt={1} fontSize={"xl"} fontWeight={"bold"}>{response?.user?.name}</Text>
+                  </Box>
+                  <Center>
+                  </Center>
+                  <CardBody>
+                    <Link to={`/profile/${response?.user?._id}`}><Button colorScheme={"blue"} w={"100%"} mb={3}>View Profile</Button></Link>
+                    <Text color={"gray.400"} fontSize={"sm"}>{response?.bio}</Text>
+                    <Tag mt={3} mb={2}>Location</Tag>
+                    <Text color={"gray.400"} fontSize={"sm"}>{response?.location || <>Not available</>}</Text>
+                    <Tag mt={3} mb={2}>Work</Tag>
+                    <Text color={"gray.400"} fontSize={"sm"}>{response?.status} {response?.company && <>at</>} {response?.company}</Text>
+                    <Tag mt={3} mb={2}>Joined</Tag>
+                    <Text color={"gray.400"} fontSize={"sm"}>{moment(response?.date).format("MMM Do YY")}</Text>
+                  </CardBody>
+                </Card>
+              )
+            }
+          </>
+        )
+      }
     </Fragment>
   )
 }
+
+export default memo(PostProfileOverviewCard);
