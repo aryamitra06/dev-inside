3312cc2b3d21133f6c79c4bf05085a455f208d29
Aryamitra Chaudhuri <caryamitra@gmail.com>
2022-12-23 20:06:40 +0530

    comment

--- client/src/pages/Profile/CreateProfile.jsx
+++ client/src/pages/Profile/CreateProfile.jsx
@@ -15,6 +15,7 @@ export default function CreateProfile() {
         if (!tokenGetter()) {
             navigate("/");
         }
+        //if profile is already created then navigate to homepage
         else if (profile?.isProfileCreated === true) {
             navigate("/");
         }
